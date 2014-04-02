1ef03af2e6a389220dada27f659318797230841d
Garret Alfert <alfert@wevelop.de>
2014-04-02 13:55:52 +0200

    Changes to work with remoteStorage 0.10

--- remotestorage-module.js
+++ remotestorage-module.js
@@ -1,40 +1,28 @@
 RemoteStorage.defineModule('myfavoritedrinks', function(privateClient) {
 
-  // Resolve a conflict where a drink is added that has also been added
-  // in another instance of the app by always taking the local one.
-  // Note: Usually it makes sense to have this behavior in the app code
-  // but in this case the module can handle it itself as this automatic
-  // resolution won't destroy any data.
-
-  privateClient.on('conflict', function(event) {
-    event.resolve('local');
-  });
-
   return {
     exports: {
 
       init: function() {
-        privateClient.cache('', true);
+        privateClient.cache('');
       },
-      
+
       on: privateClient.on,
 
       addDrink: function(name) {
-	var id = name.toLowerCase().replace(/\s|\//g, '-');
-	return privateClient.storeObject('drink', id, {
-	  name: name
-	});
+        var id = name.toLowerCase().replace(/\s|\//g, '-');
+        return privateClient.storeObject('drink', id, {
+          name: name
+        });
       },
 
-      removeDrink: /*function(id){
-        return */privateClient.remove.bind(privateClient)
-      //}
-      ,
-      
+      removeDrink: privateClient.remove.bind(privateClient),
+
       listDrinks: function() {
-	return privateClient.getAll('');
+        return privateClient.getAll('');
       }
-      
+
     }
   };
+
 });
