39759e0cd7a0e69ac7e3c4bead6018c35e6fb41a
Garret Alfert <alfert@wevelop.de>
2014-04-02 14:41:48 +0200

    Log change events

--- app.js
+++ app.js
@@ -20,6 +20,8 @@
     remoteStorage.displayWidget();
     remoteStorage.myfavoritedrinks.init();
     remoteStorage.myfavoritedrinks.on('change', function(event) {
+      console.log('change event', event);
+
       // add
       if(event.newValue && (! event.oldValue)) {
         displayDrink(event.relativePath, event.newValue.name);
