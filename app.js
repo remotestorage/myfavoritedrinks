1e90832cd0633d02f4363f154ee18f7e7b787aba
Michiel de Jong <michiel@pondersource.com>
2024-12-11 14:44:33 +0100

    Add Solid options to widget

--- app.js
+++ app.js
@@ -24,8 +24,16 @@
     // Claim read/write access for the /myfavoritedrinks category
     remoteStorage.access.claim('myfavoritedrinks', 'rw');
 
-    // Display the RS connect widget
-    var widget = new Widget(remoteStorage);
+    // Display the RS connect widget including options for Solid
+    var widget = new Widget(remoteStorage, {
+      solidProviders: {
+          providers: [
+              Widget.SOLID_COMMUNITY,
+              Widget.INRUPT
+          ],
+          allowAnyProvider: true
+      }
+    });
     widget.attach();
 
     remoteStorage.myfavoritedrinks.init();
